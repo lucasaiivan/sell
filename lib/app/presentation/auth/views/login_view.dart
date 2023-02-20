da675bca66648594468db250e6b47b2944b0bb61
Lucas Iván <83582315+lucasaiivan@users.noreply.github.com>
2023-02-20 01:36:40 -0300

    commit

--- lib/app/presentation/auth/views/login_view.dart
+++ lib/app/presentation/auth/views/login_view.dart
@@ -1,6 +1,9 @@
+import 'dart:async';
 import 'dart:math';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
 import 'package:sell/app/presentation/auth/controller/login_controller.dart';
@@ -10,8 +13,8 @@ import 'package:url_launcher/url_launcher.dart';
 // En esta pantalla de inicio de sesión, agregaremos un logotipo, dos campos de texto y firmaremos con el botón de Google
 
 // ignore: must_be_immutable
-class AuthView extends GetView<LoginController> {
-  AuthView({Key? key}) : super(key: key);
+class AuthView1 extends GetView<LoginController> {
+  AuthView1({Key? key}) : super(key: key);
 
   // var
   static Color colorFondo = Colors.deepPurple, colorAccent = Colors.deepPurple;
@@ -331,5 +334,383 @@ class DotsIndicator extends AnimatedWidget {
       mainAxisAlignment: MainAxisAlignment.center,
       children: List<Widget>.generate(itemCount, _buildDot),
     );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class AuthView extends GetView<LoginController> {
+  AuthView({Key? key}) : super(key: key);
+
+  // var
+  late bool darkMode;
+  Color colorFondo=Colors.deepPurple,
+  colorAccent=Colors.white;
+  late Size screenSize; // Obtenemos las vavriables de la dimension de la pantalla
+
+  @override
+  Widget build(BuildContext context) {
+
+    // Obtenemos los valores
+    darkMode = Theme.of(context).brightness==Brightness.dark;
+    screenSize = MediaQuery.of(context).size;
+    final AppBarTheme  appBarTheme= AppBarTheme(titleSpacing: 0, elevation: 0,toolbarHeight: 0,color: Colors.transparent ,systemOverlayStyle: darkMode?SystemUiOverlayStyle.light:SystemUiOverlayStyle.dark,iconTheme: IconThemeData(color: darkMode?Colors.white:Colors.white),titleTextStyle: TextStyle(color: darkMode?Colors.white:Colors.white));
+
+  return Theme(
+      data: Theme.of(context).copyWith(scaffoldBackgroundColor: colorFondo,primaryColor: colorAccent,appBarTheme: appBarTheme),
+      child: Scaffold(appBar: AppBar(),body: body(context: context)),
+    );
+  }
+
+  /// WIDGETS
+  Widget body({required BuildContext context}) {
+    // Definimos los estilos de colores de los botones
+    Color colorButtonText0 = Theme.of(context).brightness==Brightness.dark?Colors.white:Colors.white;
+    Color colorButton0 = Theme.of(context).brightness==Brightness.dark?Colors.deepPurple.shade400:Colors.deepPurple.shade300;
+    return Column(
+      children: [
+        Flexible( child: OnboardingIntroduction(colorAccent: colorAccent)),
+        Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [  
+              Padding(
+                padding: const EdgeInsets.only(bottom: 20),
+                child: widgetCheckAcceptPrivacyAndUsePolicy(),
+              ),
+              button(callback: controller.login,text:"Iniciar sesión con google",colorButton: colorButton0,colorText: colorButtonText0),
+              const SizedBox(height: 20.0,),
+            ],
+
+        ),
+      ],
+    );
+  }
+
+  /// WIDGETS COMPONENT
+  Widget widgetCheckAcceptPrivacyAndUsePolicy() {
+    TextStyle defaultStyle = TextStyle(color:Colors.white);
+    TextStyle linkStyle = const TextStyle(color: Colors.blue);
+
+    RichText text = RichText(
+      textAlign: TextAlign.center,
+      text: TextSpan(
+        style: defaultStyle,
+        children: <TextSpan>[
+          const TextSpan(
+              text:
+                  'Al hacer clic en INICIAR SESIÓN, usted ah leído y acepta nuestros '),
+          TextSpan(
+              text: 'Términos y condiciones de uso',
+              style: linkStyle,
+              recognizer: TapGestureRecognizer()
+                ..onTap = () async {
+                  final Uri url = Uri.parse(
+                      'https://sites.google.com/view/producto-app/t%C3%A9rminos-y-condiciones-de-uso/');
+                  if (!await launchUrl(url)) throw 'Could not launch $url';
+                }),
+          const TextSpan(text: ' así también como la '),
+          TextSpan(
+              text: 'Política de privacidad',
+              style: linkStyle,
+              recognizer: TapGestureRecognizer()
+                ..onTap = () async {
+                  final Uri url = Uri.parse(
+                      'https://sites.google.com/view/producto-app/pol%C3%ADticas-de-privacidad');
+                  if (!await launchUrl(url)) throw 'Could not launch $url';
+                }),
+        ],
+      ),
+    );
+
+    return Obx(() => Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Theme(
+            data: ThemeData.dark() ,
+            child: CheckboxListTile(
+              checkColor: Colors.white,
+              activeColor: Colors.blue,
+              title: text,
+              value: controller.getStateCheckAcceptPrivacyAndUsePolicy,
+              onChanged: (value) => controller.setStateCheckAcceptPrivacyAndUsePolicy = value!,
+            ),
+          ),
+        ));
+  }
+
+  Widget button({required Function() callback,required String text,Color colorText = Colors.white, Color colorButton = Colors.purple,double padding = 12}){
+    return Padding(
+          padding: EdgeInsets.symmetric(horizontal: padding,vertical:padding),
+          child: ElevatedButton(
+            style:ElevatedButton.styleFrom(
+              foregroundColor: Colors.white, backgroundColor: colorButton, padding: const EdgeInsets.all(16.0),shadowColor: colorButton,
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0),side: BorderSide(color:colorButton)),
+              side: BorderSide(color: colorButton),
+            ),
+            child: Text(text.toUpperCase(), style: TextStyle(color: colorText,fontSize: 18.0,fontWeight: FontWeight.bold)),
+            onPressed: callback,
+          ),
+        );
+  }
+
+  Widget buttonRoundAppBar({required void Function() onPressed,required BuildContext context,Widget ?child,required IconData icon,required EdgeInsets edgeInsets})  => Material(color: Colors.transparent,child: Center( child: Padding(padding: const EdgeInsets.all(8.0),child: Ink(decoration: ShapeDecoration(color: Brightness.dark==Theme.of(context).brightness?Colors.black:Colors.white,shape: const CircleBorder()), child: child==null?IconButton(icon: Icon(icon),color:Brightness.dark==Theme.of(context).brightness?Colors.white:Colors.black,onPressed: onPressed):child))));
+}
+
+
+
+
+class OnboardingIntroduction extends StatefulWidget {
+  OnboardingIntroduction({this.colorAccent = Colors.deepPurple,Key? key}) : super(key: key);
+
+  late Color colorAccent;
+
+  @override
+  State<OnboardingIntroduction> createState() => _OnboardingIntroductionState();
+}
+
+class _OnboardingIntroductionState extends State<OnboardingIntroduction> {
+
+  // variables para el estilo de la pantalla
+  late bool darkMode;
+  late Size screenSize;
+
+// variables para el manejo de los indicadores de progreso  
+  double indicatorProgressItem01 = 0.0;
+  double indicatorProgressItem02 = 0.0;
+  double indicatorProgressItem03 = 0.0;
+  late Timer timer;  
+  int index = 0 ;
+  late List<Widget> widgets;
+
+  // fuction  : maneja el evento de toque IZQUIERDO que cambian los valores de los indicadores de progreso y cambia la vista del item
+  void leftTouch(){ 
+    //  primer item : si el primer item esta en 0.0 y el segundo y tercer item estan en 0.0 entonces el primer item pasa a 1.0
+    if(indicatorProgressItem01 >= 0.0  &&  indicatorProgressItem02 == 0.0 &&  indicatorProgressItem03 == 0.0 ){
+      indicatorProgressItem01 = 0.0;
+      indicatorProgressItem02 = 0.0;
+      indicatorProgressItem03 = 0.0;
+      index=0;  // siguiente vista
+    }
+    // segundo item : si el primer item esta en 1.0 y el segundo item esta en 0.0 y el tercer item esta en 0.0 entonces el segundo item pasa a 1.0
+    else if( indicatorProgressItem01 == 1.0 &&  indicatorProgressItem02 >= 0.0 &&  indicatorProgressItem03 == 0.0 ){
+      indicatorProgressItem01 = 0.0;
+      indicatorProgressItem02 = 0.0;
+      indicatorProgressItem03 = 0.0;
+      index=1; //  siguiente vista
+    }
+    //  tercer item : si el primer item esta en 1.0 y el segundo item esta en 1.0 y el tercer item esta en 0.0 entonces el tercer item pasa a 1.0
+    else if( indicatorProgressItem01 == 1.0 &&  indicatorProgressItem02 == 1.0 &&  indicatorProgressItem03 >= 0.0 ){
+      indicatorProgressItem01 = 1.0;
+      indicatorProgressItem02 = 0.0;
+      indicatorProgressItem03 = 0.0;
+      index=2; //  siguiente vista
+    } 
+    // vuelve a la vista al princio
+    else{
+      indicatorProgressItem01 = 0.0;
+      indicatorProgressItem02 = 0.0;
+      indicatorProgressItem03 = 0.0;
+      index=0;
+    }
+  }
+  // fuction  : maneja el evento de toque DERECHO que cambian los valores de los indicadores de progreso
+  void rightTouch() {
+    // primer item : si el primer item esta en 0.0 y el segundo y tercer item estan en 0.0 entonces el primer item pasa a 1.0
+    if (indicatorProgressItem01 <= 1.00 && indicatorProgressItem02 == 0.0 && indicatorProgressItem03 == 0.0) {
+      indicatorProgressItem01 = 1.0;
+      indicatorProgressItem02 = 0.0;
+      indicatorProgressItem03 = 0.0;
+      index = 1; // siguiente vista
+    }
+    // segundo item : si el primer item esta en 1.0 y el segundo item esta en 0.0 y el tercer item esta en 0.0 entonces el segundo item pasa a 1.0
+    else if (indicatorProgressItem02 <= 1.00 && indicatorProgressItem01 > 0.0 && indicatorProgressItem03 == 0.0) {
+      indicatorProgressItem01 = 1.0;
+      indicatorProgressItem02 = 1.0;
+      indicatorProgressItem03 = 0.0;
+      index = 2; // siguiente vista
+    }
+    // tercer item  : si el primer item esta en 1.0 y el segundo item esta en 1.0 y el tercer item esta en 0.0 entonces el tercer item pasa a 1.0
+    else if (indicatorProgressItem03 <= 1.00 && indicatorProgressItem01 > 0.0 && indicatorProgressItem02 > 0.0) {
+      indicatorProgressItem01 = 1.0;
+      indicatorProgressItem02 = 1.0;
+      indicatorProgressItem03 = 1.0;
+      index = 3; // siguiente vista
+    }// vuelve a la vista al principio
+    else {
+      indicatorProgressItem01 = 0.0;
+      indicatorProgressItem02 = 0.0;
+      indicatorProgressItem03 = 0.0;
+      index = 0;
+    } 
+  }
+
+  void positionIndicatorLogic(){
+    // logica de los indicadores de posicion que cambiar cada sierto tiempo
+    timer = Timer.periodic( const Duration(microseconds: 50000), (timer) {
+      setState(() {
+        if(indicatorProgressItem01<1 ){
+          if( indicatorProgressItem01 >=0.1 && indicatorProgressItem01 <= 0.8 ){indicatorProgressItem01 += 0.02;}
+          else{indicatorProgressItem01 += 0.01;}
+          index=0;
+        }
+        if( indicatorProgressItem02<1 && indicatorProgressItem01>=1 ){
+          if( indicatorProgressItem02 >=0.1 && indicatorProgressItem02 <= 0.8 ){indicatorProgressItem02 += 0.02;}
+          else{indicatorProgressItem02 += 0.01;}
+          index=1;
+        }
+        if( indicatorProgressItem03<1 && indicatorProgressItem02>=1 ){
+          if( indicatorProgressItem03 >=0.1 && indicatorProgressItem03 <= 0.8 ){indicatorProgressItem03 += 0.02;}
+          else{indicatorProgressItem03 += 0.01;}
+          index=2;
+        }
+        if( indicatorProgressItem01>=1 && indicatorProgressItem02>=1 && indicatorProgressItem03>=1 ){
+          indicatorProgressItem01=0.0;
+          indicatorProgressItem02=0.0;
+          indicatorProgressItem03=0.0;
+        }
+
+      });
+    });
+  }
+
+  @override
+  void initState() {
+
+    // logicas de los indicadores de posicion
+    positionIndicatorLogic(); 
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    // Obtenemos los valores
+    darkMode = true;//Theme.of(context).brightness==Brightness.dark;
+    screenSize = MediaQuery.of(context).size;
+ 
+    // lista de widgets con las vistas
+    widgets = [
+      pageView(context:context,colorContent:Colors.transparent,textColor: darkMode?Colors.white:Colors.black,colorIcon: darkMode?Colors.white:Colors.black,iconData: Icons.monetization_on,titulo:"VENTAS",subtitulo:"Registra tus ventas de una forma simple 😊"),
+      pageView(context:context,colorContent:Colors.transparent,textColor: darkMode?Colors.white:Colors.black,colorIcon: darkMode?Colors.white:Colors.black,iconData: Icons.analytics_outlined,titulo:"TRANSACCIONES",subtitulo:"Observa las transacciones que has realizado 💰"),
+      pageView(context:context,colorContent:Colors.transparent,textColor: darkMode?Colors.white:Colors.black,colorIcon: darkMode?Colors.white:Colors.black,iconData: Icons.category,titulo:"CATÁLOGO",subtitulo:"Arma tu catálogo y controla el stock de tus productos \n 🍫🍬🥫🍾"),
+    ];
+
+    
+    return Stack(
+      children: [
+        Column(
+          children: [
+            SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20,vertical: 12),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    //  indicador de vista
+                    Flexible(
+                      child: Padding(
+                        padding: const EdgeInsets.all(1.0),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(10.0),
+                          child: LinearProgressIndicator(
+                            minHeight: 5,
+                            color: widget.colorAccent,
+                            backgroundColor: darkMode?Colors.white12:Colors.black12,
+                            value: indicatorProgressItem01,
+                          ),
+                        ),
+                      ),
+                    ),
+                    //  indicador de vista
+                    Flexible(
+                      child: Padding(
+                        padding: const EdgeInsets.all(1.0),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(10.0),
+                          child: LinearProgressIndicator(
+                            minHeight: 5,
+                            color: widget.colorAccent,
+                            backgroundColor: darkMode?Colors.white12:Colors.black12,
+                            value: indicatorProgressItem02,
+                          ),
+                        ),
+                      ),
+                    ),
+                    //  indicador de vista
+                    Flexible(
+                      child: Padding(
+                        padding: const EdgeInsets.all(1.0),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(10.0),
+                          child: LinearProgressIndicator(
+                            minHeight: 5,
+                            color: widget.colorAccent,
+                            backgroundColor: darkMode?Colors.white12:Colors.black12,
+                            value: indicatorProgressItem03,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Flexible(child: widgets[index] ),
+          ],
+        ),
+        // controlamos los toques del usuario
+        Row(
+          children: [
+            // toque izquierdo
+            Flexible(child: InkWell(onTap: leftTouch,highlightColor: Colors.transparent,splashColor: Colors.transparent)),
+            //  toque derecho
+            Flexible(child: InkWell(onTap: rightTouch,highlightColor: Colors.transparent,splashColor: Colors.transparent)),
+            //  touch
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget pageView({required BuildContext context,Color ?colorContent,Color textColor = Colors.black, AssetImage ?assetImage,IconData ?iconData,Color ?colorIcon, String titulo="",String subtitulo=""}) {
+
+    // Definimos los estilos
+    colorContent ??= Theme.of(context).brightness==Brightness.dark?Colors.white:Colors.black;
+    colorIcon ??= colorContent;
+    final estiloTitulo = TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold,color: textColor);
+    final estiloSubTitulo = TextStyle(fontSize: 20.0,color: textColor.withOpacity(0.5));
+
+    return SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: <Widget>[
+              const Spacer(),
+              assetImage!=null?Padding(padding: const EdgeInsets.all(12.0),child: Image(image: assetImage,width: screenSize.width / 2,height: screenSize.height / 2,fit: BoxFit.contain),):Container(),
+              iconData!=null?Container(padding:const EdgeInsets.all(12.0),child:Icon(iconData,size: screenSize.height*0.10,color: colorIcon)).animate(key: Key(subtitulo)).move():Container(),
+              Text(titulo,style: estiloTitulo,textAlign: TextAlign.center),
+              const SizedBox(height: 12.0),
+              Text(subtitulo,style: estiloSubTitulo,textAlign: TextAlign.center),
+              const SizedBox(height: 12.0),
+              const Spacer(),
+            ],
+          ),
+        ));
   }
 }
--- lib/app/presentation/home/controller/home_controller.dart
+++ lib/app/presentation/home/controller/home_controller.dart
@@ -218,7 +218,15 @@ void showDialogCerrarSesion() {
       TextButton(
           child: const Text('si'),
           onPressed: () async {
-            CustomFullScreenDialog.showDialog();
+            // visualizamos un diálogo alerta
+            Get.dialog(
+              const Center(
+                child: CircularProgressIndicator(),
+              ),
+              barrierDismissible: false,
+              barrierColor: const Color(0xff141A31).withOpacity(.3),
+              useSafeArea: true,
+            );
             // default values
             homeController.setProfileAccountSelected=ProfileAccountModel(creation: Timestamp.now());
             homeController.setProfileAdminUser = UserModel ();
@@ -232,7 +240,8 @@ void showDialogCerrarSesion() {
             final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
             Future.delayed(const Duration(seconds: 2)).then((_) {
               firebaseAuth.signOut().then((value) async {
-                CustomFullScreenDialog.cancelDialog();
+                // finalizamos el diálogo alerta
+                Get.back();
               });
             });
           }),
