f76018bbd5ef0df847573fe7dbe7a80a8945d093
Lógica Booleana <logica.booleana.2019@gmail.com>
2023-04-07 20:19:54 -0300

    refactory web app

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/lucasaiivan/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/lucasaiivan/StudioProjects/sell"
+export "FLUTTER_ROOT=C:\src\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Usuario\Documents\GitHub\sell"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=1.0.1"
-export "FLUTTER_BUILD_NUMBER=2"
+export "FLUTTER_BUILD_NAME=1.0.2"
+export "FLUTTER_BUILD_NUMBER=3"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
