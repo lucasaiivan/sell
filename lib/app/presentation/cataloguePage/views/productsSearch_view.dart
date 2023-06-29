1512e3bf4081bd4c3abb195665507be1ba4a92fb
Lucas Iván <83582315+lucasaiivan@users.noreply.github.com>
2023-06-28 22:35:39 -0300

    commit

--- lib/app/presentation/cataloguePage/views/productsSearch_view.dart
+++ lib/app/presentation/cataloguePage/views/productsSearch_view.dart
@@ -1,6 +1,5 @@
 
-import 'package:cached_network_image/cached_network_image.dart';
-import 'package:clipboard/clipboard.dart';
+import 'package:cached_network_image/cached_network_image.dart'; 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
