35b530e0cf30842849998b653e4a8589b4444729
Lógica Booleana <logica.booleana.2019@gmail.com>
2024-02-18 11:16:13 -0300

    commit

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/lucasaiivan/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/lucasaiivan/StudioProjects/sell"
+export "FLUTTER_ROOT=C:\src\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Usuario\Documents\GitHub\sell"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.1.0"
